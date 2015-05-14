94a6559b66bb709696414a6375877a913beb0b7f
Michael Luo <miczluo@gmail.com>
2015-05-14 15:25:55 -0700

    bootstrap template

---
+++ app.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var http = require('http');
+var handlebars = require('express-handlebars');
+
+var routes = require('./routes/index');
+
+var app = express();
+
+// view engine setup
+app.engine('handlebars', handlebars());
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'views'));
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+http.createServer(app).listen(3000, function() {
+    console.log('Express server listening on port ' + 3000);
+});
+
+module.exports = app;
