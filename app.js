b84f48bcffe6191ca03f47440ae7a61d60d09837
leta415 <leta.he.415@gmail.com>
2015-05-21 14:15:49 -0700

    deleted favicon

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
+// var favicon = require('serve-favicon');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var http = require('http');
