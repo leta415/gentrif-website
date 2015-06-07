35ccfd29cf163318462346bc1dcf5a11f75333f3
Andie Romero <andriana@beltranfamily.com>
2015-06-07 14:56:12 -0700

    hover effects about page

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -33,4 +33,66 @@ $('a[href*=#]:not([href=#])').click(function() {
     }
 });
 
-function
+$('.aboutInside#demo') 
+  .mouseenter(function() {
+
+    $('img.aboutImg#demo').hide(100);
+    $('h2#demo').hide(100);
+    $('p.hiddenText#demo').css('display', 'block');
+    // $('.aboutInside#demo').addClass('hvr-outline-in');
+  })
+
+  .mouseleave(function () {
+    $('img.aboutImg#demo').show(100);
+    $('h2#demo').show(100);
+    $('p.hiddenText#demo').css('display', 'none');
+
+  });
+
+$('.aboutInside#market') 
+  .mouseenter(function() {
+
+    $('img.aboutImg#market').hide(100);
+    $('h2#market').hide(100);
+    $('p.hiddenText#market').css('display', 'block');
+    // $('.aboutInside#demo').addClass('hvr-outline-in');
+  })
+
+  .mouseleave(function () {
+    $('img.aboutImg#market').show(100);
+    $('h2#market').show(100);
+    $('p.hiddenText#market').css('display', 'none');
+
+  });
+
+  $('.aboutInside#land') 
+  .mouseenter(function() {
+
+    $('img.aboutImg#land').hide(100);
+    $('h2#land').hide(100);
+    $('p.hiddenText#land').css('display', 'block');
+    // $('.aboutInside#demo').addClass('hvr-outline-in');
+  })
+
+  .mouseleave(function () {
+    $('img.aboutImg#land').show(100);
+    $('h2#land').show(100);
+    $('p.hiddenText#land').css('display', 'none');
+
+  });
+
+  $('.aboutInside#culture') 
+  .mouseenter(function() {
+
+    $('img.aboutImg#culture').hide(100);
+    $('h2#culture').hide(100);
+    $('p.hiddenText#culture').css('display', 'block');
+    // $('.aboutInside#demo').addClass('hvr-outline-in');
+  })
+
+  .mouseleave(function () {
+    $('img.aboutImg#culture').show(100);
+    $('h2#culture').show(100);
+    $('p.hiddenText#culture').css('display', 'none');
+
+  });
